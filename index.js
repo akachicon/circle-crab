ca512c090677ebacbd09edccbcf19585fab7c4bc
Dan Z <akachicon@gmail.com>
2018-01-23 12:00:09 +0300

    Add procfile and change port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const app = express();
+const express = require('express'),
+  app = express(),
+  PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.listen('3000', console.log('server is running'));
+app.listen(PORT, console.log(`server is running on port ${ PORT }`));

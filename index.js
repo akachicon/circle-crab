91a6ff8ef0b40959262c14fcbf0b669d354e89cb
Dan Z <akachicon@gmail.com>
2018-01-23 12:19:14 +0300

    Add maxAge property with val of 3 days to serve-static option object

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express'),
   app = express(),
   PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static('public', { maxAge: 3 * 60 * 60 * 1000 }));
 
 app.listen(PORT, console.log(`server is running on port ${ PORT }`));
